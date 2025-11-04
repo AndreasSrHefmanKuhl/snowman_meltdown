24931fd520dc72990320093c4d4e9fe22c5be484
heavsmac <hefbolzen@gmail.com>
2025-11-04 08:45:28 +0100

    outsourced stages into ascii_art.py, imported ascii_art.py in snowman.py

--- snowman.py
+++ snowman.py
@@ -1,35 +1,9 @@
 import random
+from ascii_art import STAGES
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
-STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___  
-     /___\\ 
-     """
- ]
+
 
 MAX_MISTAKES = len(STAGES)-1
 
